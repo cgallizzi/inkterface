9526fea3813182128b583ccc0a022091ceb31379
NaKyle <nakyle.wright@gmail.com>
2025-10-09 19:34:48 -0700

    Add option to install self as service on linux.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
+#include <filesystem>
 #include <iostream>
+#include <thread>
 
 #include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QDebug>
+#include <QDir>
 #include <QLoggingCategory>
 #include <QTimer>
-#include <thread>
 
 #include "config.h"
 #include "frunk.hpp"
@@ -14,6 +16,27 @@
 #define EXIT_SUCCESS 0
 
 using namespace Qt::Literals::StringLiterals;
+
+static const QString SERVICE_TEMPLATE{u"[Unit]\n"_s
+                                      u"Description=mango-frunk\n"_s
+                                      u"After=network.target\n"_s
+                                      u"\n"_s
+                                      u"[Service]\n"_s
+                                      u"Type=simple\n"_s
+                                      u"ExecStart=%1%2\n"_s
+                                      u"Restart=on-failure\n"_s
+                                      u"\n"_s
+                                      u"[Install]\n"_s
+                                      u"WantedBy=default.target\n"_s};
+
+QString getExecutablePath()
+{
+    try {
+        return {std::filesystem::canonical("/proc/self/exe").c_str()};
+    } catch (const std::exception &ex) {
+        return {""};
+    }
+}
 
 int runHeadless(int argc, char *argv[], [[maybe_unused]] const QString &name)
 {
@@ -37,6 +60,41 @@ int runHeadless(int argc, char *argv[], [[maybe_unused]] const QString &name)
     return exitCode;
 }
 
+int installService([[maybe_unused]] int argc, [[maybe_unused]] char *argv[],
+                   [[maybe_unused]] const QString &name)
+{
+    auto exePath = getExecutablePath();
+    if (exePath.isEmpty()) {
+        qWarning() << "Can only install service on linux hosts!";
+        return 1;
+    }
+    auto svcDef =
+        SERVICE_TEMPLATE.arg(exePath).arg(name.isEmpty() ? u""_s : u" --name %1"_s.arg(name));
+    auto dir = QDir::home();
+    if (!dir.mkpath(u".config/systemd/user"_s)) {
+        qWarning() << "Failed to create systemd user config directory!";
+        return 1;
+    }
+    dir.cd(u".config/systemd/user"_s);
+    if (dir.exists(u"mango-frunk.service"_s)) {
+        qDebug() << "Uninstalling old service definition...";
+        system("systemctl --user stop mango-frunk.service");
+        system("systemctl --user disable mango-frunk.service");
+        dir.remove(u"mango-frunk.service"_s);
+    }
+    QFile f{dir.filePath(u"mango-frunk.service"_s)};
+    if (!f.open(QFile::WriteOnly)) {
+        qWarning() << "Failed to create systemd user service definition!";
+        return 1;
+    }
+    f.write(svcDef.toLatin1());
+    f.close();
+    system("systemctl --user daemon-reload");
+    system("systemctl --user enable mango-frunk.service");
+    system("systemctl --user start mango-frunk.service");
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     QLoggingCategory::setFilterRules("qt.bluetooth* = false");
@@ -52,6 +110,9 @@ int main(int argc, char *argv[])
                                   u"name"_s};
     parser.addOption(nameOption);
 
+    QCommandLineOption installOption{u"install"_s, u"Install as a systemd service (LINUX ONLY)."_s};
+    parser.addOption(installOption);
+
     QStringList args;
     for (int i = 0; i < argc; ++i) {
         args << QString(argv[i]);
@@ -64,5 +125,9 @@ int main(int argc, char *argv[])
         qDebug() << "Using provided name:" << name;
     }
 
-    return runHeadless(argc, argv, name);
+    if (parser.isSet(installOption)) {
+        return installService(argc, argv, name);
+    } else {
+        return runHeadless(argc, argv, name);
+    }
 }
